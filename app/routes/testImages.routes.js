fab9156e1927cf45e9dc6a9dd638dfd6df190b17
Byron Alexander Herrera Martínez <74020380+alexanderhm95@users.noreply.github.com>
2023-07-07 12:55:46 -0500

    random testImages

--- app/routes/testImages.routes.js
+++ app/routes/testImages.routes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { upload } = require("../utils/helpers/multer.helper");
 const testImagesController = require("../controllers/testImages.controller");
-const checkAuth = require("../utils/validators/authCheck.js")
 
 router.post("/", upload.single("singleFile"), testImagesController.create);
-router.get("/", checkAuth, testImagesController.findAll);
+router.get("/", testImagesController.findAll);
 router.get("/paginated", testImagesController.findAllPaginated);
 router.get("/:id", testImagesController.findOne);
 router.put("/:id", upload.single("singleFile"), testImagesController.update);
