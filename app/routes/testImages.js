0f6508758eb1a67e6ae3e0337f44fa517d7de291
Byron Alexander Herrera Martínez <74020380+alexanderhm95@users.noreply.github.com>
2023-07-17 20:36:28 -0500

    cargar rutas

--- app/routes/testImages.js
+++ app/routes/testImages.js
@@ -6,7 +6,7 @@ const checkAuth = require("../utils/validators/authCheck")
 
 
 router.post("/",          checkAuth(['ADMIN','DECE']),  upload.single("singleFile"), testImagesController.create);
-router.get("/",           checkAuth(['ADMIN','DECE']),   testImagesController.findAll);
+router.get("/",           checkAuth(['STUDENT','ADMIN','DECE']),   testImagesController.findAll);
 router.get("/paginated",  checkAuth(['ADMIN','DECE']),   testImagesController.findAllPaginated);
 router.get("/:id",        checkAuth(['ADMIN','DECE']),  testImagesController.findOne);
 router.put("/:id",        checkAuth(['ADMIN','DECE']),  upload.single("singleFile"), testImagesController.update);
