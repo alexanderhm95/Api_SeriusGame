ff4872bd24586bf2e0745a4f1af94594b76c1598
Byron Alexander Herrera Martínez <74020380+alexanderhm95@users.noreply.github.com>
2023-08-02 20:16:26 -0500

    Control de funciones

--- app/utils/validators/authCheck.js
+++ app/utils/validators/authCheck.js
@@ -1,19 +1,23 @@
 const { verifyToken, decodeToken } = require("../helpers/handle.jwt");
+const User = require("../../models/user.model");
 
 const checkAuth = (roles) => async (req, res, next) => {
   try {
     //TODO: authorization: Bearer 1010101010101001010100
     const token = req.headers.authorization.split(" ").pop(); //TODO:123123213
-    const tokenData = await verifyToken(token);
+    const tokenData = verifyToken(token);
     console.log(
       "Token decodificado:",
       decodeToken(token).payload.role,
       "llega: ",
       roles
     );
+
     const decodedToken = decodeToken(token);
 
-    if (tokenData && roles.includes(decodedToken.payload.role)) {
+    const existUser = await User.findById(decodedToken.payload.user);
+
+    if (tokenData && roles.includes(decodedToken.payload.role) && existUser) {
       next();
     } else {
       res.status(403);
