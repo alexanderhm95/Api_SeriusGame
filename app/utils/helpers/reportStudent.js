629007ebeaa28e3c55dc8549b3c470d7080a8030
Byron Alexander Herrera Martínez <74020380+alexanderhm95@users.noreply.github.com>
2024-03-20 17:03:31 -0500

    Update reportStudent.js

--- app/utils/helpers/reportStudent.js
+++ app/utils/helpers/reportStudent.js
@@ -74,7 +74,7 @@ const obtenerDatosInforme = async (id) => {
     respuestas: testStudent.answers || null,
   };
 };
-
+console.log("Buscando valores", testStudent.respuestas)
 // Función para generar el contenido del informe con los datos obtenidos.....
 const generarContenidoInforme = async (casoData) => {
   const content = [];
